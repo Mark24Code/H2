1372212f20ce67f4c3ffa55c13bd72fd36a2228c
Mark24 <mark.zhangyoung@qq.com>
2016-04-19 23:02:07 +0800

    move

--- H2/Account/views.py
+++ H2/Account/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 
 def index(request):
     print(111111111111111);
-    return render_to_response('index2.html',{})
+    return render_to_response('index.html',{})
 
 # def login(request):
 #     return render_to_response('login.html',{})

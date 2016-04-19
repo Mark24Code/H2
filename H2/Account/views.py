4765656fbaacd46c22495fec2323e622b5f2cadd
Mark24 <mark.zhangyoung@qq.com>
2016-04-19 23:24:49 +0800

    add login

--- H2/Account/views.py
+++ H2/Account/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 
+# def index(request):
+#     print(111111111111111);
+#     return render_to_response('index.html',{})
 def index(request):
     print(111111111111111);
-    return render_to_response('index.html',{})
+    return render_to_response('login.html',{})
 
 # def login(request):
 #     return render_to_response('login.html',{})

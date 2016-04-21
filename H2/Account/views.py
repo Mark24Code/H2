741b24b03e3c08293f184d7cb0c9793d707a65a2
Mark24 <mark.zhangyoung@qq.com>
2016-04-21 11:30:43 +0800

    why

--- H2/Account/views.py
+++ H2/Account/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.contrib import auth
 from django.contrib.auth.models import Group, User
-from django.contrib.auth import authenticate
 from Account.models import UserProfile
 from django.contrib.auth.decorators import login_required
 import json
@@ -13,60 +13,59 @@ def login(request):
     '''
     账号注册与登录
     '''
-    #注册
-    if request.POST.get('_method','')=="put":
-        try:
+    if request.POST:
+        #注册
+        if request.POST.get('_method','')=="put":
             print('api/account:PUT')
             username = request.POST['username']
             password = request.POST['password']
-            user = User.objects.create(username = username)
-            user.set_password(password)
-            user.save()
-            data={"code":200}
+            try:
+                user = User.objects.create_user(username = username,password=password)
+            except Exception as err:
+                print(err)
             return HttpResponseRedirect('/login/')
-        except Exception as err:
-            print(err)
-            data = {"code":500}
-            return JsonResponse(data)
 
-    #登录
-    if request.POST.get('_method','')=='post':
-        try:
+
+        #登录
+        if request.POST.get('_method','')=='post':
             print('api/account:POST')
             username = request.POST['username']
             password = request.POST['password']
 
-            user = authenticate(username=username,password=password)
-            print('sadddddddddddd')
+            user = auth.authenticate(username=username,password=password)
             print(user)
-            if user is not None:
-                print(11111111111111)
-                if user.is_active:
-                    print(33333333333333)
-                    request.session['user_id'] = user.id
-                    print(333444444444444)
-                    return HttpResponseRedirect('/index/')
-                else:
-                    return HttpResponseRedirect('/login/')
-            else:
-                print(2222222222222222)
-                return HttpResponseRedirect('/login/')
-
-        except Exception as err:
-            print(err)
-            return HttpResponseRedirect('/login/')
+            if user and user.is_active:
+                auth.login(request,user)
+                request.session['user_id'] = user.id
+                print(9999)
+                #return render_to_response('index.html',{})
+                return user_router(user)
 
     print('account:GET')
     return render_to_response('login.html',{})
+    #return render_to_response('index.html',{})
+
+def user_router(user):
+    '''
+    基于不同角色跳转
+    '''
+    print(44444444)
+    if user.is_superuser:
+        return HttpResponseRedirect('/')
+    else:
+        print(111111144444444)
+        return HttpResponseRedirect('/')
+
 
 def logout(request):
-    try:
-        del request.session['user_id']
-    except KeyError:
-        pass
+    '''
+    注销
+    '''
+    del request.session['user_id']
+    auth.logout(request)
     return HttpResponseRedirect('/login/')
 
-
+@login_required()
 def index(request):
-    print(999999)
+    print('index:')
     return render_to_response('index.html',{})
--- H2/H2/urls.py
+++ H2/H2/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from Account import views as AccountViews
 urlpatterns = [
-    # url(r'^$', AccountViews.index),
+    url(r'^$', AccountViews.index),
     url(r'^index/$', AccountViews.index),
     url(r'^login/$', AccountViews.login),
     url(r'^admin/', include(admin.site.urls)),

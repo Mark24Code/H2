599f18549a64e0868b2d1c5fc8eb76803853ab1b
Mark24 <mark.zhangyoung@qq.com>
2016-04-21 18:03:03 +0800

    登录逻辑

--- H2/Account/views.py
+++ H2/Account/views.py
@@ -1,111 +1,86 @@
-from django.http import Http404
-from django.http import HttpResponse,HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.http import JsonResponse
-from django.shortcuts import render
-from django.shortcuts import render_to_response
+import json
+import traceback
 from django.contrib import auth
 from django.contrib.auth.models import Group, User
-from Account.models import UserProfile
 from django.contrib.auth.decorators import login_required
-import json
+
+from django.http import Http404
+from django.http import JsonResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+
+from core import jsonresponse
+from Account.models import UserProfile
 
 
 def login(request):
-    if request.POST:
-        print('login:POST')
-        if request.POST.get('_method') == 'put':
-            print('api/login:PUT')
-            return HttpResponseRedirect(reverse('index'))
-        if request.POST.get('_method') == 'post':
-            print('api/login:POST')
-            return HttpResponseRedirect(reverse('index'))
-    else:
-        print('login:GET')
+    """
+    登录和注册
+    """
+    #注册
+    if request.POST.get('_method','') == 'put':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            User.objects.create_user(username=username,password=password)
+        except:
+            traceback.print_exc()
 
+        resp = jsonresponse.creat_response(200)
+        data = {
+            'url':'/login/'
+        }
+        resp.data = data
+        return resp.get_response()
+    #登录
+    elif request.POST.get('_method','') == 'post':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user and user.is_active:
+            auth.login(request,user)
+            request.session['user_id'] = user.id
+            reload_url = user_router(user)
+            data = {
+                'url':reload_url
+            }
+            resp = jsonresponse.creat_response(200)
+            resp.data = data
+            return resp.get_response()
+    else:
         return render_to_response('login.html',{})
 
-# def logout(request):
-#     if request.POST:
-#         print('login:POST')
-#     else:
-#         print('login:GET')
-#         return render_to_response('logout.html',{})
+
+def logout(request):
+    """
+    注销
+    """
+    del request.session['user_id']
+    auth.logout(request)
+    data = {
+        'url':'/login/'
+    }
+    resp = jsonresponse.creat_response(200)
+    resp.data = data
+    return resp.get_response()
 
 
-
-def index(request):
-    if request.POST:
-        print('index:POST')
+def user_router(user):
+    """
+    角色跳转
+    """
+    if user.is_superuser:
+        return "/"
     else:
-        print('index:GET')
-        return render_to_response('index.html',{})
+        return "/"
 
 
-
-
-
-
-# def login(request):
-#     '''
-#     账号注册与登录
-#     '''
-#     if request.POST:
-#         print('AAAAAAAAAA')
-#         return HttpResponseRedirect('/')
-#         ##注册
-#         #if request.POST.get('_method','')=="put":
-#         #    print('api/account:PUT')
-#         #    username = request.POST['username']
-#         #    password = request.POST['password']
-#         #    try:
-#         #        user = User.objects.create_user(username = username,password=password)
-#         #    except Exception as err:
-#         #        print(err)
-#         #    return HttpResponseRedirect('/login/')
-
-
-#         ##登录
-#         #if request.POST.get('_method','')=='post':
-#         #    return HttpResponseRedirect('/')
-#             #print('api/account:POST')
-#             #username = request.POST['username']
-#             #password = request.POST['password']
-
-#             #user = auth.authenticate(username=username,password=password)
-#             #print(user)
-#             #if user and user.is_active:
-#             #    auth.login(request,user)
-#             #    request.session['user_id'] = user.id
-#             #    print(9999)
-#             #    #return render_to_response('index.html',{})
-#             #    return user_router(user)
-
-#     print('account:GET')
-#     return render_to_response('login.html',{})
-#     #return render_to_response('index.html',{})
-
-# def user_router(user):
-#     '''
-#     基于不同角色跳转
-#     '''
-#     print(44444444)
-#     if user.is_superuser:
-#         return HttpResponseRedirect('/')
-#     else:
-#         print(111111144444444)
-#         return HttpResponseRedirect('/')
-
-
-# def logout(request):
-#     '''
-#     注销
-#     '''
-#     del request.session['user_id']
-#     auth.logout(request)
-#     return HttpResponseRedirect('/login/')
-
-# @login_required()
-# def index(request):
-#     print('index:')
-#     return render_to_response('index.html',{})
+@login_required()
+def index(request):
+    """
+    首页
+    """
+    print('index')
+    return render_to_response('index.html',{})

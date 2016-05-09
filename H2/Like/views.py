f791c565a7a8746b7f7199a3d24b9031ace0f283
Mark24 <zhangyang@weizoom.com>
2016-05-09 10:21:16 +0800

    like btn ok

--- H2/Like/views.py
+++ H2/Like/views.py
@@ -22,56 +22,58 @@ from Comment.models import Comment
 
 @login_required()
 def like_api(request):
-    # if request.POST.get('_method','') == 'put':
-    #     user_id = str(request.user.id)
-    #     like_title = request.POST.get('like_title','')
-    #     like_content = request.POST.get('like_content','')
+    if request.POST.get('_method','') == 'put':
+        user_id = str(request.user.id)
+        blog_id = request.POST.get("blog_id","")
+        print("PUT:user_id",user_id,',blog_id',blog_id)
+        try:
+            like = Like(
+                user_id=user_id,
+                blog_id=blog_id)
+            like.save()
+        except Exception as e:
+            print(e)
 
-    #     like = Like(
-    #         user_id=user_id,
-    #         title=like_title,
-    #         content=like_content)
-    #     like.save()
-    #     like_id = str(like.id)
+        resp = jsonresponse.creat_response(200)
+        data = {
+            # 'url':'/blogs/?id={user_id}'.format(user_id=user_id)
+        }
+        resp.data = data
+        return resp.get_response()
 
-    #     resp = jsonresponse.creat_response(200)
-    #     data = {
-    #         'url':'/likes/?id={user_id}'.format(user_id=user_id)
-    #     }
-    #     resp.data = data
-    #     return resp.get_response()
+    elif request.POST.get('_method','') == 'delete':
+        user_id = str(request.user.id)
+        blog_id = request.POST.get("blog_id","")
+        print("DELETE:user_id",user_id,',blog_id',blog_id)
 
-    # elif request.POST.get('_method','') == 'delete':
-    #     user_id = str(request.user.id)
-    #     like_id = request.POST.get('like_id',0)
-    #     like_title = request.POST.get('like_title','')
-    #     like_content = request.POST.get('like_content','')
+        try:
+            Like.objects.filter(user_id=user_id,blog_id=blog_id).delete()
+        except Exception as e:
+            print(e)
 
-    #     like = Like.objects.filter(id=like_id,user_id=user_id)
-    #     like.update(
-    #         title=like_title,
-    #         content=like_content)
-    #     resp = jsonresponse.creat_response(200)
-    #     data = {
-    #         'url':'/likes/like/?id={user_id}&like_id={like_id}'.format(user_id=user_id,like_id=like_id)
-    #     }
-    #     resp.data = data
-    #     return resp.get_response()
-    # elif request.GET:
-    print('>>>FFF')
+        resp = jsonresponse.creat_response(200)
+        data = {
+            # 'url':'/blogs/?id={user_id}'.format(user_id=user_id)
+        }
+        resp.data = data
+        return resp.get_response()
+    elif request.GET:
 
-    user_id = str(request.user.id)
-    blog_id = request.GET.get('blog_id','')
-    try:
-        like_data = Like.objects.filter(blog_id=blog_id).order_by('id')
-        like = len(like_data)
-    except:
-        like = 0
-    print(like)
-
-    resp = jsonresponse.creat_response(200)
-    data = {
-        'like':like
-    }
-    resp.data = data
-    return resp.get_response()
+        user_id = str(request.user.id)
+        blog_id = request.GET.get('blog_id','')
+        liked = False
+        try:
+            like_data = Like.objects.filter(blog_id=blog_id).order_by('id')
+            like = len(like_data)
+            like_user_id = [str(like.user_id) for like in like_data]
+            if str(user_id) in like_user_id:
+                liked = True
+        except:
+            like = 0
+        resp = jsonresponse.creat_response(200)
+        data = {
+            'like':like,
+            'liked':liked
+        }
+        resp.data = data
+        return resp.get_response()
